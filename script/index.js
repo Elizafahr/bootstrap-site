545d420ddbe3949d14afea9970f44efc8c16ad74
Elizafahr <124281054+Elizafahr@users.noreply.github.com>
2023-07-27 09:15:27 +0500

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,14 +1,45 @@
-// document.getElementById('main-action-btn').onclick = function () {
-//     document.getElementById('products').scrollIntoView({ behavior: 'smooth' })
-//   }
-  
-  //перенос по ссылкам в навигации
-  let links = document.querySelectorAll('.nav-item > a')
-  for (let i = 0; i < links.length; i++) {
-    links[i].onclick = function () {
-      document
-        .getElementById(links[i].getAttribute('data-link'))
-        .scrollIntoView({ behavior: 'smooth' })
-    }
+// var swiper = new Swiper(".swiper-container", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
+
+
+
+var swiper = new Swiper(".swiper-container-menu", {
+  slidesPerView: 5, 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+const swiperEl = document.querySelector('swiper-container')
+
+const params = {
+  injectStyles: [`
+  .swiper-pagination-bullet {
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    line-height: 20px;
+    font-size: 12px;
+    color: #000;
+    opacity: 1;
+    background: rgba(0, 0, 0, 0.2);
   }
-  
+
+  .swiper-pagination-bullet-active {
+    color: #fff;
+    background: #007aff;
+  }
+  `],
+  pagination: {
+    clickable: true,
+    renderBullet: function (index, className) {
+    },
+  },
+}
+
+Object.assign(swiperEl, params)
+
+swiperEl.initialize();
